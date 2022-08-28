dc844ba0ed1e5a6bf8ee496198c79366cd3d40b7
Lorena Morales <Lorenarm.999@gmail.com>
2022-08-27 23:48:19 -0600

    added started const package and fs/write file func

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('README.md', json.stringify(fileName, data), (err) =>
+    err ? console.log(err) : console.log("read me successfully created! :)" )
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() {}
