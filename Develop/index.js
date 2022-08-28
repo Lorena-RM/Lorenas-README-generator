5b9840a8a943c0faf3426c72d321b319f1d6fa82
Lorena Morales <Lorenarm.999@gmail.com>
2022-08-28 00:10:06 -0600

    test working//starter application

--- Develop/index.js
+++ Develop/index.js
@@ -1,29 +1,28 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(answers)//fileName, data {
-    fs.writeFile('README.md', json.stringify(answers), (err) =>
-    err ? console.log(err) : console.log("read me successfully created! :)" )
-    );
+function writeToFile(answers) {
+  //fileName, data
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt(questions)
-    .then(answers)
-    writeToFile(answers)
+  inquirer.prompt(questions).then((answers) => {
+    fs.writeFile("README.md", JSON.stringify(answers), (err) =>
+      err ? console.log(err) : console.log("read me successfully created! :)")
+    );
+  });
 }
 
 // Function call to initialize app
