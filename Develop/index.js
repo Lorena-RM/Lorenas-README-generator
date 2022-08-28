506c33f40cf95a7d93c1384b49e70b7cb6225907
Lorena Morales <Lorenarm.999@gmail.com>
2022-08-27 23:49:30 -0600

    added starter question/will be changed(test only)

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
