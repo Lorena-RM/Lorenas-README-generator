bf8addb696b39b90443ef726ce36df5862eaae8f
Lorena Morales <Lorenarm.999@gmail.com>
2022-09-11 21:13:34 -0600

    added correct questions for HW

--- Develop/index.js
+++ Develop/index.js
@@ -7,21 +7,63 @@ const questions = [
   {
     type: "input",
     name: "name",
-    message: "What is your name?",
+    message: "What is the Title of your project?",
   },
+  {
+    type: "input",
+    name: "Desc",
+    message: "What is the Description of your project?",
+  },
+  {
+    type: "input",
+    name: "Isntallation",
+    message: "What is required to intall and run this project?",
+  },
+  {
+    type: "input",
+    name: "Uasage",
+    message: "write out the intended purpose and usage of this project?",
+  },
+  {
+    type: "list",
+    name: "Licence",
+    message: "Which License do you want to use?",
+    choices: ["MIT", "Unlicence", "Mozilla", "IBM", "Apache"]
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "who contributed to this proejct?",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "What are the tests intended for this project?",
+  },
+  {
+    type: "input",
+    name: "GithubUsername",
+    message: "what is your Github URL?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(answers) {
   //fileName, data
+  fs.writeFile("README.md", JSON.stringify(answers), (err) =>
+      err ? console.log(err) : console.log("read me successfully created! :)")
+    );
 }
 
 // TODO: Create a function to initialize app
 function init() {
   inquirer.prompt(questions).then((answers) => {
-    fs.writeFile("README.md", JSON.stringify(answers), (err) =>
-      err ? console.log(err) : console.log("read me successfully created! :)")
-    );
+    writeToFile(answers)
   });
 }
 
